7c623764cd594bde0767b92e9b83e23b89524836
Brittany Huber <brittanyleighhuber@gmail.com>
2019-10-29 20:48:32 -0600

    Added some questions into variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,17 +8,55 @@
 //What color is the Time Stone? Green, purple, blue, orange
 //Thanos' goal throughout the course of the Infinity Saga is to collect the 6 infinity stones. In what device does Thanos put these Infinity Stones to harness their collective powers? Infinity Gauntlet, Infinity Belt, Infinity Pants, Infinity Necklace
 //In Avengers: End Game, we learn that Tony Stark and Pepper Potts have had a daughter in the 5 years after "the snap." What is their daughter's name? Morgan, Scarlett, Natasha, Wanda
+//Which Avenger is getting a 4th solo film in Phase 4 of the MCU? Thor, Captain America, Iron Man, Hulk
 
 
 
 //Function to start the game
 
 
+//Define questions and answers
+var questions = [
+    {
+        question: "Who was the first Avenger?",
+        options: ["Captain America", "Iron Man", "Black Widow", "Thor"],
+        answer: 0,
+        image: "assets/images/captain.gif"
+
+    },
+
+    {
+        question: "What is the name of Tony Stark's Father? Howard, Stephen, Chris, Peter",
+        options: ["Chris", "Stephen", "Howard", "Peter"],
+        answer: 2,
+        image: "assets/images/howard.jpg"
+    },
+
+    {
+        question: "On what planet is the Soul Stone found?",
+        options: ["Morag", "Vormir", "Earth", "Mars"],
+        answer: 1,
+        image: "assets/images/vormir.jpeg"
+    },
+
+    {
+        question: "What is Peter Quill's superhero name?",
+        options: ["Iron Man", "Black Panther", "Star-lord", "Vision"],
+        answer: 2,
+        image: "assets/images/star-lord.gif"
+    }
+]
+
+
 //Function that counts down time 
 
-//Function to switch questions
+
+//Function to switch questions/update html
+
 
 //Store answers to keep score
 
+
 //Play game again
+
 
