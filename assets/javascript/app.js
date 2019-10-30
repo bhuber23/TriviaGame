c9584d7c3d1a2f183ab5919e98a913dac540b8aa
Brittany Huber <brittanyleighhuber@gmail.com>
2019-10-29 19:39:40 -0600

    Pseudo coded questions/answers, some pseudo code for app

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+//Questions and answers
+//Who was the first Avenger? Captain America, Iron Man, Black Widow, Thor
+//What is the name of Tony Stark's Father? Howard, Stephen, Chris, Peter
+//On what planet is the Soul Stone found? Vormir, Earth, Mars, Morag
+//What is Peter Quill's superhero name? Star-lord, Iron Man, Black Panther, Vision
+//The Infinity Saga spanned from 2008 to 2019. How many MCU films were part of this saga? 23, 16, 22, 20
+//Spiderman makes his debut in the MCU in Captain America: Civil War. Which actor plays this (best) version of Peter Parker? Tom Holland, Tobey Maguire, Andrew Garfield, Chris Evans
+//What color is the Time Stone? Green, purple, blue, orange
+//Thanos' goal throughout the course of the Infinity Saga is to collect the 6 infinity stones. In what device does Thanos put these Infinity Stones to harness their collective powers? Infinity Gauntlet, Infinity Belt, Infinity Pants, Infinity Necklace
+//In Avengers: End Game, we learn that Tony Stark and Pepper Potts have had a daughter in the 5 years after "the snap." What is their daughter's name? Morgan, Scarlett, Natasha, Wanda
+
+
+
+//Function to start the game
+
+
+//Function that counts down time 
+
+//Function to switch questions
+
+//Store answers to keep score
+
+//Play game again
+
